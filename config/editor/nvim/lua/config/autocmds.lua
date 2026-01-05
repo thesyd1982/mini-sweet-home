58959e58d3d7a4920466d24c1fcc213db77c8edc
thesyd1982 <salah.yacine.douakha@gmail.com>
2026-01-05 19:43:22 +0000

    fix:autocomand treesitter

--- config/editor/nvim/lua/config/autocmds.lua
+++ config/editor/nvim/lua/config/autocmds.lua
@@ -5,42 +5,44 @@
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("thesyd-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("thesyd-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.sql", "*.sqlite" },
-	command = "set filetype=sql",
+    pattern = { "*.sql", "*.sqlite" },
+    command = "set filetype=sql",
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	desc = "Fixe treesitter",
-	callback = function()
-		local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-		ts_update()
-	end,
+    desc = "Fix treesitter",
+    callback = function()
+        -- La correction ici : on appelle directement la fonction update()
+        require("nvim-treesitter.install").update({ with_sync = true })
+    end,
 })
 
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*.templ",
-	callback = function()
-		vim.cmd("TSBufEnable highlight")
-	end,
+    pattern = "*.templ",
+    callback = function()
+        vim.cmd("TSBufEnable highlight")
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.tmpl", "*.gotmpl", "*.gohtml" },
-	command = "set filetype=gotmpl",
+    pattern = { "*.tmpl", "*.gotmpl", "*.gohtml" },
+    command = "set filetype=gotmpl",
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.md",
-	callback = function()
-		vim.opt_local.spell = false
-	end,
+    pattern = "*.md",
+    callback = function()
+        vim.opt_local.spell = false
+    end,
 })
+
+-- Enregistrement du type de fichier pour gotmpl
 vim.treesitter.language.register("gotmpl", { "gotexttmpl", "gohtmltmpl" })
